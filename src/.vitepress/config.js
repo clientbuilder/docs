21a48b312c78d1b94f4638b1cdc00b99e0a5bb24
Georgi Karagogov <georgi.karagogov@gmail.com>
2022-02-11 02:32:09 +0200

    Init

---
+++ src/.vitepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'Client Builder Documentation',
+    description: 'Next level productivity tool',
+    head: [
+    ],
+    themeConfig: {
+    },
+}
